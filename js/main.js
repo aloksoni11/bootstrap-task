6ba1833fcfe818ec6dd4e692132a85c96437cae8
aloksoni11 <sonialok5777@gmail.com>
2019-04-15 13:08:52 +0530

    first init

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function first() {
+    document.getElementById("secondH1").innerHTML = "C";
+    document.getElementById("ThirdH1").innerHTML = "B";
+
+}
+
+function second() {
+    document.getElementById("firstH1").innerHTML = "B";
+    document.getElementById("ThirdH1").innerHTML = "A";
+}
+function third(){
+    document.getElementById("firstH1").innerHTML = "C";
+    document.getElementById("secondH1").innerHTML = "B";
+}
---
+++ js/task6.js
@@ -0,0 +1,8 @@
+function sum() {
+    var x = document.getElementById("num1").textContent;
+    var y = document.getElementById("num2").textContent;
+    var z = +x + +y;
+    document.getElementById("ansbar").innerHTML=z;
+    
+}
+    
